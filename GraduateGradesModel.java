099c43f1f3257fb2225febd0edec91c9339f342c
alicehamori <a.hamori@student.maastrichtuniversity.nl>
2025-10-04 23:55:45 +0200

    implement mode for course grades

--- GraduateGradesModel.java
+++ GraduateGradesModel.java
@@ -204,6 +204,28 @@ public class GraduateGradesModel {
 
         return median;
     }
-    public static double getCourseMode(int courseId) {}
+
+    public static double getCourseMode(int courseId) {
+        // Calculate mode of grades of a specific course
+        double mode;
+        // Count the frequencies of all grades (6.0, 7.0, 8.0, 9.0, 10.0)
+        // index 0 is 6.0, index 1 is 7.0, etc.
+        // So we need to subtract 6 from the grade to get its corresponding index in the
+        int[] gradeFrequencies = new int[5];
+        for (int studentId = 0; studentId < grades.length; studentId++) {
+            gradeFrequencies[(int)grades[studentId][courseId] - 6] += 1;
+        }
+
+        // Searches highest frequency
+        int indexHighest = 0;
+        for (int i = 0; i < gradeFrequencies.length; i++) {
+            if (gradeFrequencies[indexHighest] < gradeFrequencies[i]) {
+                indexHighest = i;
+            }
+        }
+        mode = indexHighest + 6;    // automatically cast as double
+
+        return mode;
+    }
 }
 
