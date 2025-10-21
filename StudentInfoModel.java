caf34dfd851ecfe3a361d6619deebd6edbb35946
alicehamori <a.hamori@student.maastrichtuniversity.nl>
2025-10-22 00:20:53 +0200

    add source .csv and empty class

---
+++ StudentInfoModel.java
@@ -0,0 +1,7 @@
+/** Reads CurrentGrades.csv, stores it internally, computes statistics
+    on itself via public methods.
+    As of now it is a static class, and meant to serve as an "interface"
+    for accessing student info data fields.
+ */
+public class StudentInfoModel {
+}
