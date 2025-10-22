7173d93ffc9c8b1f756c33bb0048911952a576b0
alicehamori <a.hamori@student.maastrichtuniversity.nl>
2025-10-22 11:47:17 +0200

    add entry point

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import java.util.Locale;
+
+/** Entry point of the project. You can access the data models from here.*/
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+    }
+}
