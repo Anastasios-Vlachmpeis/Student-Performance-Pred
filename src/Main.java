3c0b9d56b68db5edec1049f2fc33c66f970a08ba
alicehamori <a.hamori@student.maastrichtuniversity.nl>
2025-10-24 14:21:56 +0200

    add messages declaring what message is answered before answer is written to console

--- src/Main.java
+++ src/Main.java
@@ -23,18 +23,22 @@ public class Main {
           # STEP 1 #
           ###########*/
         // Question 1
+        System.out.println("Answering Step 1 - Question 1");
         GraduateGradesModel.printBestAndWorstCourse();
 
         // Question 2
+        System.out.println("Answering Step 1- Question 2");
         GraduateGradesModel.printCumLaudeStudents();
 
         // Q3: Are there courses that seem similar or related?
         //     Find top 10 most similar course pairs.
+        System.out.println("Answering Step 1 - Question 3");
         int TOP_K = 10; // Take the top 10 course pairs with the highest correlation
         GraduateGradesModel.printTopKCorrelatedCoursePairs(TOP_K);
 
          // Q4: Which students performed significantly better in the difficult courses, compared to the easy ones?
          //     Find the top 10 best performing ones.
+        System.out.println("Answering Step 1 - Question 4");
         GraduateGradesModel.analyzeStudentPerformanceHardVsEasy();
 
          /*###########
@@ -43,24 +47,29 @@ public class Main {
         // Q0: Computing basic statistics we did for step 1
         //     Adjusted Q1 from graduate grades for current grades
         //     (Compute some of the same statistics you did in step 1 for current grades
+        System.out.println("Answering step 2 - some statistics we did on graduate grades applied to current grades");
+        System.out.println("(Not a real question)");;
         CurrentGradesModel.printHardestAndEasiestCourses();
 
         // Q1:
         // it is done with graphics will be in report
+        System.out.println("Answering Step 2 - Question 1");
 
         // Q2:
         CurrentGradesModel.getGraduatingStudents();
+        System.out.println("Answering Step 2 - Question 2");
 
         // Q3 (extra): Adjusted Q3 from graduate grade for current grades!
+        System.out.println("Answering Step 2 - Question 3");
         CurrentGradesModel.printTopKCorrelatedCoursePairsIgnoreNG(TOP_K);
         // Q4 (extra): Adjusted Q4 for current grades
+        System.out.println("Answering Step 2 - Question 4");
         CurrentGradesModel.analyzeStudentPerformanceHardVsEasyNG();
 
-        // QPrediction:
-        // Missing
 
-        // QPrediction: Alice approach
+        // QPrediction: Predicting number of graduates
         //              Monte Carlo simulate NGs based on passing rates
+        System.out.println("Answering Step 2 - Prediction Question");
         System.out.println("Prediction of number of graduates via Monte Carlo Simulation and resits:");
         int ITERATIONS = 10000;
         int MAX_RESITS = 3;
@@ -71,6 +80,7 @@ public class Main {
           # STEP 3 #
           ###########*/
         // The actual question of step 3: finding the rules for grade prediction
+        System.out.println("Answering step 3 - Rules for grade prediction");
         printBestRulesForGradePrediction();
 
 
