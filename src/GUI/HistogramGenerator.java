4179fe4acd578a88d2ce5e061eed5afebfcac105
DenizEmek <92950553+WirexDae@users.noreply.github.com>
2025-11-24 18:07:39 +0100

    The template for choosing the dataset and settin up very basic filter for the x-axis is done, however it is only done for Bar Chart right now

--- src/GUI/HistogramGenerator.java
+++ src/GUI/HistogramGenerator.java
@@ -1,9 +1,13 @@
 package GUI;
 
-import javafx.scene.chart.Chart;
+import datamodels.CurrentGradesModel;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.Alert;
 
 public class HistogramGenerator {
-    public Chart createChart() {
-        return null;
-    }
+//CURRENTLY NOT WORKING HAVE PROBLEMS WITH PULLING THE DATA
+
 }
