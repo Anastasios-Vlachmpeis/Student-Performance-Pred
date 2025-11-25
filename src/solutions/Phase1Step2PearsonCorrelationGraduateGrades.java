f7898d879186ef75b0a5a91dd9aff942a597213f
Hámori, Alice (Stud. DACS) <a.hamori@student.maastrichtuniversity.nl>
2025-11-25 13:50:49 +0000

    Edit Phase1Step2PearsonCorrelationGraduateGrades.java

--- src/solutions/Phase1Step2PearsonCorrelationGraduateGrades.java
+++ src/solutions/Phase1Step2PearsonCorrelationGraduateGrades.java
@@ -22,7 +22,7 @@ public class Phase1Step2PearsonCorrelationGraduateGrades {
      * pairs as the most "similar" courses, while skipping NG entries.
      */
 
-    // Compute PearsonCorrelation correlation for a pair of courses, considering only students with grades in both
+    // Compute pearson correlation for a pair of courses, considering only students with grades in both
     static double pearsonBetweenCoursesIgnoreNG(int i, int j) {
         // Gathers pairs of grades only when both courses have a grade for the same student
         ArrayList<Double> gradesA = new ArrayList<>();
