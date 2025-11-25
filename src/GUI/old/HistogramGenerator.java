856c5f47d575fe47fb907edd499befa90281e5a6
Julian <j.langer@student.maastrichtuniversity.nl>
2025-11-25 12:04:53 +0100

    Copied the template from BarChartGenerator.

--- src/GUI/old/HistogramGenerator.java
+++ src/GUI/old/HistogramGenerator.java
@@ -5,13 +5,14 @@ import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
+import javafx.scene.control.Alert;
 
 import java.util.Map;
 import java.util.TreeMap;
 
 public class HistogramGenerator {
 
-    public BarChart<String, Number> createChart() {
+    public BarChart<String, Number> createChart(String xAxisData, String yAxisData, int xAxisFilterStart, int xAxisFilterEnd, int yAxisFilterStart, int yAxisFilterEnd) {
 
         //Count how many courses have each NG value
         Map<Integer, Integer> frequencies = new TreeMap<>();
@@ -39,6 +40,64 @@ public class HistogramGenerator {
 
         // Data series
         XYChart.Series<String, Number> series = new XYChart.Series<>();
+        series.setName(yAxisData + " " + xAxisData);
+
+        //If chosen X axis is per Course
+        if (xAxisData.equals("")) {
+
+            //Alert if the inputs are not in range
+            if (xAxisFilterStart < 0 || xAxisFilterEnd > CurrentGradesModel.courseCount || xAxisFilterStart >= xAxisFilterEnd) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Invalid Range");
+                alert.setHeaderText("Filter range is out of bounds!");
+                alert.setContentText("Valid range: 0 to " + CurrentGradesModel.courseCount);
+                alert.show();
+            }
+
+            for (int i = xAxisFilterStart; i <= xAxisFilterEnd; i++) {
+                String courseName = CurrentGradesModel.getCourseName(i);
+                Number value = switch (yAxisData) {
+                    case "Number of Courses" -> CurrentGradesModel.getCourseNG(i);
+                    case "Mean of Grades" -> CurrentGradesModel.calcCourseMean(i);
+                    case "Mode of Grades" -> CurrentGradesModel.calcCourseMode(i);
+                    case "Median of Grades" -> CurrentGradesModel.calcCourseMedian(i);
+                    default -> 0;
+                };
+
+                if(value.doubleValue() >= yAxisFilterStart && value.doubleValue() <= yAxisFilterEnd) {
+                    series.getData().add(new XYChart.Data<>(courseName, value)); }}
+
+        }
+
+        //If chosen X axis is Per Student
+        if (xAxisData.equals("Per Student")) {
+
+
+            //Alert if the inputs are not in range
+            if (xAxisFilterStart < 0 || xAxisFilterEnd > CurrentGradesModel.studentCount || xAxisFilterStart >= xAxisFilterEnd) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Invalid Range");
+                alert.setHeaderText("Filter range is out of bounds!");
+                alert.setContentText("Valid range: 0 to " + CurrentGradesModel.studentCount);
+                alert.show();
+            }
+
+            for (int i = xAxisFilterStart; i <= xAxisFilterEnd; i++) {
+                String studentNumber = "Student " + i;
+
+                Number value = switch (yAxisData) {
+                    case "Number of NG" -> CurrentGradesModel.getStudentNG(i);
+                    case "Mean of Grades" -> CurrentGradesModel.calcStudentMean(i);
+                    case "Mode of Grades" -> CurrentGradesModel.calcStudentMode(i);
+                    case "Median of Grades" -> CurrentGradesModel.calcStudentMedian(i);
+                    default -> 0;
+                };
+
+                if (value.doubleValue() >= yAxisFilterStart && value.doubleValue() <= yAxisFilterEnd) {
+                    series.getData().add(new XYChart.Data<>(studentNumber, value));
+                }
+            }
+        }
 
         for (Map.Entry<Integer, Integer> entry : frequencies.entrySet()) {
             int ngValue = entry.getKey();
