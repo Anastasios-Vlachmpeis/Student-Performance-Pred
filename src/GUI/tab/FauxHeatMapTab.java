f7802a76678133dcd85ab7fc0e3a32f74126fb53
alicehamori <alicehamori@gmail.com>
2025-12-03 04:07:08 +0100

    heatmap comments

--- src/GUI/tab/FauxHeatMapTab.java
+++ src/GUI/tab/FauxHeatMapTab.java
@@ -7,15 +7,11 @@ import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.ScatterChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
-import javafx.util.Pair;
 import tools.Color;
 import tools.PearsonCorrelation;
 
-import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -63,20 +59,21 @@ public class FauxHeatMapTab {
         }
 
 
+        // add buckets to the chart in the form of series (1 series corresponds to 1 bucket)
         for (int i = 0; i < correlationBuckets.length; i++) {
             XYChart.Series series = correlationBuckets[i];
             if (series.getData().isEmpty()) {
                 continue;
             }
-
             sc.getData().add(series);
         }
 
+        // CSS blackmagic, redefine markers on scatter chart and color them based on heatmap color distribution
         List<Integer> usedSeriesNumbers = new ArrayList<>();
         for (int seriesNumber = 0; seriesNumber < sc.getData().size(); seriesNumber++) {
             boolean flagIsFirst = true;
             for (Node seriesNode : sc.lookupAll(".series" + seriesNumber)) {
-                seriesNode.setStyle("-fx-background-color: " + Color.heatmapRGBRedBoosted(-1, 1, -0.2 + seriesNumber * 0.2) + "; -fx-shape: \"M 10 10  v10.0 h 10.0  v-10  Z\";-fx-padding: 6px;");
+                seriesNode.setStyle("-fx-background-color: " + Color.heatmapRGBinCSS(-1, 1, -0.2 + seriesNumber * 0.2) + "; -fx-shape: \"M 10 10  v10.0 h 10.0  v-10  Z\";-fx-padding: 6px;");
                 if (flagIsFirst) {
                     usedSeriesNumbers.add(seriesNumber);
                     flagIsFirst = false;
@@ -84,6 +81,8 @@ public class FauxHeatMapTab {
             }
 
         }
+        // CSS black magic but with legend items, making the marker symbol disappear and color them according
+        // to the heatmap distribution
         int counter = 0;
         Set<Node> legendItems = sc.lookupAll("Label.chart-legend-item");
         for (Node legendNode : legendItems) {
@@ -92,7 +91,7 @@ public class FauxHeatMapTab {
             // removes the default icon from the legend
             label.setGraphic(null);
             // colors the legend entry matching the corresponding heatmap color
-            label.setStyle("-fx-background-color: " + Color.heatmapRGBRedBoosted(-1, 1, -0.2 + seriesNumber * 0.2));
+            label.setStyle("-fx-background-color: " + Color.heatmapRGBinCSS(-1, 1, -0.2 + seriesNumber * 0.2));
             counter++;
         }
 
@@ -138,6 +137,7 @@ public class FauxHeatMapTab {
         }
 
 
+        // add buckets to the chart in the form of series (1 series corresponds to 1 bucket)
         for (int i = 0; i < correlationBuckets.length; i++) {
             XYChart.Series series = correlationBuckets[i];
             if (series.getData().isEmpty()) {
@@ -147,11 +147,12 @@ public class FauxHeatMapTab {
             sc.getData().add(series);
         }
 
+        // CSS blackmagic, redefine markers on scatter chart and color them based on heatmap color distribution
         List<Integer> usedSeriesNumbers = new ArrayList<>();
         for (int seriesNumber = 0; seriesNumber < sc.getData().size(); seriesNumber++) {
             boolean flagIsFirst = true;
             for (Node seriesNode : sc.lookupAll(".series" + seriesNumber)) {
-                seriesNode.setStyle("-fx-background-color: " + Color.heatmapRGBRedBoosted(-1, 1, -0.2 + seriesNumber * 0.2) + "; -fx-shape: \"M 10 10  v10.0 h 10.0  v-10  Z\";-fx-padding: 6px;");
+                seriesNode.setStyle("-fx-background-color: " + Color.heatmapRGBinCSS(-1, 1, -0.2 + seriesNumber * 0.2) + "; -fx-shape: \"M 10 10  v10.0 h 10.0  v-10  Z\";-fx-padding: 6px;");
                 if (flagIsFirst) {
                     usedSeriesNumbers.add(seriesNumber);
                     flagIsFirst = false;
@@ -167,7 +168,7 @@ public class FauxHeatMapTab {
             // removes the default icon from the legend
             label.setGraphic(null);
             // colors the legend entry matching the corresponding heatmap color
-            label.setStyle("-fx-background-color: " + Color.heatmapRGBRedBoosted(-1, 1, -0.2 + seriesNumber * 0.2));
+            label.setStyle("-fx-background-color: " + Color.heatmapRGBinCSS(-1, 1, -0.2 + seriesNumber * 0.2));
             counter++;
         }
 
